65cee22b924205596dac939865f9a1f9902af265
Johannes Filter <hi@jfilter.de>
2018-08-14 17:41:36 +0200

    add website

---
+++ website/brunch-config.js
@@ -0,0 +1,39 @@
+exports.files = {
+  javascripts: {
+    joinTo: {
+      'js/app.js': /^(app)/,
+      'js/vendor.js': /^node_modules/,
+    },
+  },
+  stylesheets: { joinTo: 'css/app.css' },
+};
+
+exports.plugins = {
+  pleeease: {
+    sass: true,
+    autoprefixer: {
+      browsers: ['> 1%'],
+    },
+  },
+};
+
+exports.modules = {
+  autoRequire: {
+    'js/app.js': ['js/initialize'],
+  },
+};
+
+exports.npm = {
+  globals: {
+    jQuery: 'jquery',
+    $: 'jquery',
+    bootstrap: 'bootstrap',
+  },
+  styles: {
+    bootstrap: ['dist/css/bootstrap.css'],
+  },
+};
+
+exports.paths = {
+  public: '../docs',
+};
